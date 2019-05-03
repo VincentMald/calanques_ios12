5c555fa1232f5070fa4a20e959ab595c3be23fe8
vincent <vincentmald@gmail.com>
2019-05-03 11:34:58 +0200

    create myAnnotation

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -9,27 +9,68 @@
 import UIKit
 import MapKit
 
-class ControllerAvecCarte: UIViewController {
+class ControllerAvecCarte: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var calanques: [Calanque] = CallanqueCollection().all()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        mapView.delegate = self
+        addAnnotations()
+    }
+    
+    func addAnnotations(){
+        for calanque in calanques {
+            
+            //Annotations de bases
+            //let annotation = MKPointAnnotation()
+            //annotation.coordinate = calanque.coordonnee
+            //annotation.title = calanque.nom
+            //mapView.addAnnotation(annotation)
+            
+            //Annotations custom
+            let annotation = MonAnnotation(calanque)
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "reuseId"
+        
+        if annotation.isKind(of: MKUserLocation.self){
+            return nil
+        }
+        
+        if let anno = annotation as? MonAnnotation {
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+            if annotationView == nil{
+                annotationView = MonAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+                
+                //annotationView = MKAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+                //annotationView?.image = UIImage(named: "placeholder")
+                //annotationView?.canShowCallout = true
+                return annotationView
+            } else {
+                return annotationView
+            }
+        }
+        return nil
     }
     
     
-    @IBAction func segmentedChanged(_ sender: Any) {
-    }
-    
-    @IBAction func getPosition(_ sender: UISegmentedControl) {
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex{
         case 0: mapView.mapType = MKMapType.standard
         case 1: mapView.mapType = .satellite
         case 2: mapView.mapType = .hybrid
         default: break
         }
+    }
+    
+    @IBAction func getPosition(_ sender: Any) {
+        
     }
     
 }
---
+++ Les Calanques/Vue/MonAnnotationView.swift
@@ -0,0 +1,69 @@
+//
+//  MonAnnotationView.swift
+//  Les Calanques
+//
+//  Created by Vincent Maldonado on 03/05/2019.
+//  Copyright © 2019 Vincent Maldonado. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MonAnnotationView: MKAnnotationView {
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupAnnotation()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder :aDecoder)
+        setupAnnotation()
+    }
+    
+    func setupAnnotation(){
+        image = UIImage(named:"placeholder")
+        canShowCallout = true
+        leftCalloutAccessoryView = setupLeft()
+        rightCalloutAccessoryView = setupRight()
+        detailCalloutAccessoryView = setupCenter()
+    }
+    
+    func setupLeft() -> UIButton{
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "distance"), for: .normal)
+        button.addTarget(self, action: #selector(gps), for: .touchUpInside)
+        return button
+    }
+    
+    func setupRight() -> UIButton{
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "detail"), for: .normal)
+        button.addTarget(self, action: #selector(detail), for: .touchUpInside)
+        return button
+    }
+    
+    func setupCenter() -> UIView? {
+        guard let anno = annotation as? MonAnnotation else {return nil}
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 125, height: 125))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        
+        let imageView = UIImageView(frame: view.bounds)
+        imageView.image = anno.calanque.image
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        
+        return view
+    }
+    
+    @objc func detail(){
+        
+    }
+    
+    @objc func gps(){
+        
+    }
+}
